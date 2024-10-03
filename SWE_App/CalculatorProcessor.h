86c568eab2948d88c8a8049eec01b5b9123f6a6c
Terry <ellisonterry4@gmail.com>
2024-10-02 21:44:55 -0400

    added hint comments for CalcualatorProcessor

--- SWE_App/CalculatorProcessor.h
+++ SWE_App/CalculatorProcessor.h
@@ -1,6 +1,10 @@
 #include <string>
 #include "ButtonIDDefines.h"
 #pragma once
+
+/// <summary>
+/// Singleton that handles the processing for the Calculator
+/// </summary>
 class CalculatorProcessor
 {
 	static CalculatorProcessor* _instance;
@@ -11,12 +15,22 @@ protected:
 	float EvaluateExpression(const std::string& expression) const;
 	float Process(const std::string left, const char mathOp, const std::string right) const;
 public:
+	/// <summary>
+	/// Returns a reference to the processor
+	/// </summary>
+	/// <returns></returns>
 	static CalculatorProcessor* GetInstance();
-
+	/// <summary>
+	/// Releases control of the reference to the processor
+	/// </summary>
 	static void ResetInstance();
-
+	/// <summary>
+	/// Parses an expression and returns its numerical value
+	/// </summary>
+	/// <param name="input"></param>
+	/// <returns></returns>
 	float ParseExpression(std::string input) const;
-
+	
 	float EvaluateTrigFunction(const unsigned int id, const std::string text) const;
 
 	CalculatorProcessor(CalculatorProcessor& other) = delete;
